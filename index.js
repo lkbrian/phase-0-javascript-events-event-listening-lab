76c1161d8c801461f7289b8d81c924709e1b66cd
lkbrian <lkbrian742@gmail.com>
2023-12-07 15:48:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById("button");
 function addingEventListener() {
+    function listen(){
+        alert(`just clicked my ${input.id}`);
+    }
+  input.addEventListener("click", listen);
 }
+addingEventListener();
